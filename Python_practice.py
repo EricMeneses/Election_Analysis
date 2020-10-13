1e54674b5601c7165ee4ae7f7507c312f1031ca5
Eric Meneses <ericmeneses@Erics-MacBook-Air.local>
2020-10-12 20:03:38 -0700

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,8 @@
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {3345} number of votes. "
+    f"The total number of votes in the election was {23123}. "
+    f"You received {3345 / 23123 * 100}% of the total votes.")
+
+print(message_to_candidate)
